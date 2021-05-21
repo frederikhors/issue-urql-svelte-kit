e29606009e5c5595471ed7b30bf274cdd6fd8141
frederikhors <41120635+frederikhors@users.noreply.github.com>
2021-05-21 10:31:34 +0200

    add code from @urql-svelte example

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,45 +1,40 @@
-<script lang="ts">
-	import Header from '$lib/Header/index.svelte';
-	import '../app.css';
+<script>
+	import { initClient, gql, operationStore, query } from '@urql/svelte';
+
+	initClient({
+		url: 'https://trygql.formidable.dev/graphql/basic-pokedex'
+	});
+
+	const pokemons = operationStore(
+		gql`
+			query ($skip: Int!) {
+				pokemons(limit: 10, skip: $skip) {
+					id
+					name
+				}
+			}
+		`,
+		{ skip: 0 }
+	);
+
+	query(pokemons);
+
+	function nextPage() {
+		$pokemons.variables = { skip: $pokemons.variables.skip + 10 };
+	}
 </script>
 
-<Header />
-
-<main>
-	<slot />
-</main>
-
-<footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-</footer>
-
-<style>
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 1024px;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 40px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 40px 0;
-		}
-	}
-</style>
+<div>
+	{#if $pokemons.fetching}
+		Loading...
+	{:else if $pokemons.error}
+		Oh no... {$pokemons.error.message}
+	{:else}
+		<ul>
+			{#each $pokemons.data.pokemons as pokemon}
+				<li>{pokemon.name}</li>
+			{/each}
+		</ul>
+	{/if}
+	<button on:click={nextPage}>Next Page</button>
+</div>
